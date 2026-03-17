744381c8d09b64fce7b9570c58838e319a928a69
Hiago 7k <hiago7k@gmail.com>
2026-03-17 20:03:01 -0300

    transformand o json em Lista com a resposta

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using (HttpClient httpClient = new HttpClient())
     try
     {
         string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
-        var filmes = JsonSerializer.Deserialize<List<Filmes>>();
+        var filmes = JsonSerializer.Deserialize<List<Filmes>>(resposta)!;
     }
     catch (Exception ex)
     {
