e25c39e25117f9b7c7b2cf6070dc6411b68dfd5c
Hiago 7k <hiago7k@gmail.com>
2026-03-17 19:51:39 -0300

    passando nosso endpoint

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿using (HttpClient httpClient = new HttpClient()) 
 {
+
+    string  resposta  = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
 }
