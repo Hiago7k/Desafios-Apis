685d9400901b310bc4b2c9dcfeb947abad128980
Hiago 7k <hiago7k@gmail.com>
2026-03-17 19:52:45 -0300

    tratando nossa API

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿using (HttpClient httpClient = new HttpClient()) 
 {
 
-    string  resposta  = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+    try
+    {
+        string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro ao realizar requisicao na API {ex:Message}");
+    }
+
+
+    
 }
