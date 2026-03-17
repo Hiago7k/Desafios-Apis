98f9bd99730d28105afe995edbf8bbeae2652dc6
Hiago 7k <hiago7k@gmail.com>
2026-03-17 19:56:48 -0300

    realizando anotacoes das proprieades

--- Modelos/Filmes.cs
+++ Modelos/Filmes.cs
@@ -1,5 +1,15 @@
-﻿namespace Desafios_Apis.Modelos;
+﻿using System.Text.Json.Serialization;
+
+namespace Desafios_Apis.Modelos;
 
 internal class Filmes
 {
+    [JsonPropertyName("title")]
+    public string NomeDoFilme { get; set; }
+    [JsonPropertyName("year")]
+    public int AnoDoFilme { get; set; }
+    [JsonPropertyName("crew")]
+    public string Equipe { get; set; }
+    [JsonPropertyName("rank")]
+    public int Rank { get; set; }
 }
